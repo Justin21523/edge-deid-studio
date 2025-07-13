c3a7fe33d4944d9f196ebe73129e86b5a9adbb11
Justin21523 <b0979865617@gmail>
2025-07-13 22:22:52 +0800

    feat: Add image preprocessing to improve OCR accuracy

--- src/file_proc/image_ocr.py
+++ src/file_proc/image_ocr.py
@@ -7,8 +7,13 @@ class OCRProcessor:
         self.ocr_engine = PaddleOCR(use_angle_cls=True, lang=lang, use_gpu=False)
 
     def preprocess_image(self, image: np.ndarray) -> np.ndarray:
-        # TODO: 影像前處理
-        return image
+        # 自動對比度調整 (CLAHE)
+        lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(lab)
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+        l2 = clahe.apply(l)
+        lab2 = cv2.merge([l2, a, b])
+        return cv2.cvtColor(lab2, cv2.COLOR_LAB2BGR)
 
     def extract_text(self, image: np.ndarray) -> str:
         processed = self.preprocess_image(image)
