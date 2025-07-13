21cbec8b136dcfa33b2320affbb03b43f0576914
Justin21523 <b0979865617@gmail>
2025-07-13 22:36:02 +0800

    feat: Implement redact_image() with text and visual entity masking

--- src/image_deid/pipeline.py
+++ src/image_deid/pipeline.py
@@ -20,9 +20,18 @@ class ImagePIIPipeline:
             'plates': self.plate_detector.detect(image)
         }
         return results
-    
+
     def redact_image(self, image: np.ndarray, results: dict) -> np.ndarray:
-        # TODO: apply redaction masks
+        # 文字遮蔽
+        for box in results['text_boxes']:
+            x1, y1, x2, y2 = box
+            roi = image[y1:y2, x1:x2]
+            text = self._recognize_text(roi)
+            if self._is_sensitive(text):
+                image = self.redactor.mask(image, box)
+        # 視覺實體遮蔽
+        for entity in results['faces'] + results['plates']:
+            image = self.redactor.blur(image, tuple(entity))
         return image
 
     def _recognize_text(self, roi: np.ndarray) -> str:
