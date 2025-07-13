f20adf2a30832da1e139da4383d0d3771470a3ba
Justin21523 <b0979865617@gmail>
2025-07-13 22:37:41 +0800

    feat: Add FaceDetector class skeleton

---
+++ src/image_deid/face_detector.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+class FaceDetector:
+    def __init__(self):
+        # TODO: load DNN model
+        pass
+
+    def detect(self, image: np.ndarray, min_confidence=0.7) -> list:
+        # TODO: implement face detection
+        return []
