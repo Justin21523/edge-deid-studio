2bbb31537f0e35de6bf70bc2552b875f2981dc14
Justin21523 <b0979865617@gmail>
2025-07-13 22:40:10 +0800

    feat: Add Redactor Class Skeleton

---
+++ src/image_deid/redactor.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+class Redactor:
+    def __init__(self):
+        self.method = "pixelate"
+
+    def mask(self, image: np.ndarray, box: tuple) -> np.ndarray:
+        # TODO
+        return image
+
+    def blur(self, image: np.ndarray, box: tuple) -> np.ndarray:
+        # TODO
+        return image
