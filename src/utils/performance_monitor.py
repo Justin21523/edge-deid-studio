d99ad0d526f217d0eee1b6cbdae7a9e79ac20ffe
Justin21523 <b0979865617@gmail>
2025-07-13 22:42:37 +0800

    feat: Add ResourceMonitor class skeleton

---
+++ src/utils/performance_monitor.py
@@ -0,0 +1,12 @@
+import psutil
+
+class ResourceMonitor:
+    @staticmethod
+    def current_load() -> float:
+        # TODO
+        return 0.0
+
+    @staticmethod
+    def adjust_processing(config: dict) -> dict:
+        # TODO
+        return config
