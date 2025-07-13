8caec99a4ae4990207fee8cc3fe1e1e0ec6b3ee5
Justin21523 <b0979865617@gmail>
2025-07-13 22:40:52 +0800

    feat: Implement pixelate masking in Redactor.mask()

--- src/image_deid/redactor.py
+++ src/image_deid/redactor.py
@@ -6,7 +6,13 @@ class Redactor:
         self.method = "pixelate"
 
     def mask(self, image: np.ndarray, box: tuple) -> np.ndarray:
-        # TODO
+        x1, y1, x2, y2 = box
+        roi = image[y1:y2, x1:x2]
+        # 像素化
+        factor = 8
+        small = cv2.resize(roi, ( (x2-x1)//factor, (y2-y1)//factor ), interpolation=cv2.INTER_NEAREST)
+        pixelated = cv2.resize(small, (x2-x1, y2-y1), interpolation=cv2.INTER_NEAREST)
+        image[y1:y2, x1:x2] = pixelated
         return image
 
     def blur(self, image: np.ndarray, box: tuple) -> np.ndarray:
