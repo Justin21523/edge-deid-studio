b61ad6cdffdfbaafd086f07dc97ad4c3ba888a64
penpenpenguin <linjiazhen0128@gmail.com>
2025-07-15 00:31:12 +0800

    First edition UI

---
+++ src/ui/AnoniMe/UploadFileInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace AnoniMe
+{
+    public class UploadFileInfo : INotifyPropertyChanged
+    {
+        private string _fileName = string.Empty;
+        private double _uploadProgress;
+        private string _uploadStatus = string.Empty;
+
+        public string FileName
+        {
+            get => _fileName;
+            set
+            {
+                _fileName = value;
+                OnPropertyChanged(nameof(FileName));
+            }
+        }
+
+        public double UploadProgress
+        {
+            get => _uploadProgress;
+            set
+            {
+                _uploadProgress = value;
+                OnPropertyChanged(nameof(UploadProgress));
+            }
+        }
+
+        public string UploadStatus
+        {
+            get => _uploadStatus;
+            set
+            {
+                _uploadStatus = value;
+                OnPropertyChanged(nameof(UploadStatus));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
