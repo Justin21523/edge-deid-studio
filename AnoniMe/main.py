006dc73985bde13aa3ec1a933f234cfb20c29359
Justin21523 <b0979865617@gmail.com>
2025-07-22 19:12:27 +0800

    refactor(pkg): merge legacy AnoniMe/pii_models into unified deid_pipeline

--- AnoniMe/main.py
+++
@@ -1,7 +0,0 @@
-from pii_models.detector import detect_pii
-
-text = "Jennifer Lawrence 的電話是 +912345578，email 是 lucas@mail.com。"
-entities = detect_pii(text)
-
-for ent in entities:
-    print(f"{ent['label']}: {ent['text']}")
