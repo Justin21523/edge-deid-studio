7fff2f02e6f94342c7a8c6d07838d2517b3f9b2c
penpenpenguin <linjiazhen0128@gmail.com>
2025-07-25 00:36:27 +0800

    Incomplete result page

--- src/ui/AnoniMe/ViewModels/ResultViewModel.cs
+++ src/ui/AnoniMe/ViewModels/ResultViewModel.cs
@@ -1,38 +1,74 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using AnoniMe.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace AnoniMe.ViewModels
 {
 
     public partial class ResultViewModel : ObservableObject
     {
-        public ObservableCollection<string> ProcessedFileNames { get; } = new()
-        {
-            "report_masked.pdf",
-            "notes_cleaned.txt"
-        };
+        public ObservableCollection<PreviewFileInfo> PreviewFiles { get; } = new();
 
-        [ObservableProperty]
-        private string filePreviewText = "這是預覽內容，模擬網頁展示效果。";
+        public ObservableCollection<string> ProcessedFileNames { get; } = new();
 
         [RelayCommand]
-        public void Download() { /* 實作下載邏輯 */ }
+        public void Download()
+        {
+            // TODO: 實作下載邏輯（可遍歷 PreviewFiles）
+        }
 
         [RelayCommand]
         public void GoHome()
         {
             if (App.MainAppWindow?.Content is Frame rootFrame)
-            {
                 rootFrame.Navigate(typeof(HomePage));
-            }
         }
 
+        public async Task LoadFilesAsync(IEnumerable<StorageFile> files)
+        {
+            PreviewFiles.Clear();
+
+            foreach (var file in files)
+            {
+                var props = await file.GetBasicPropertiesAsync();
+                var info = new PreviewFileInfo
+                {
+                    FileName = file.Name,
+                    FileType = file.FileType,
+                    FileSizeDisplay = $"{props.Size / 1024.0 / 1024.0:F2} MB",
+                    IsPdf = file.FileType.Equals(".pdf", StringComparison.OrdinalIgnoreCase),
+                    IsDoc = file.FileType.Equals(".doc", StringComparison.OrdinalIgnoreCase) ||
+                            file.FileType.Equals(".docx", StringComparison.OrdinalIgnoreCase)
+                };
+
+                if (info.IsPdf)
+                {
+                    info.PdfUri = $"file:///{file.Path}";
+                    info.PreviewText = $"PDF 預覽：{file.Name}";
+                }
+                else if (info.IsDoc)
+                {
+                    using var stream = await file.OpenStreamForReadAsync();
+                    using var reader = new StreamReader(stream, Encoding.UTF8);
+                    info.PreviewText = await reader.ReadToEndAsync();
+                }
+                else
+                {
+                    info.PreviewText = "尚未支援此格式";
+                }
+
+                PreviewFiles.Add(info);
+            }
+        }
     }
+
 }
