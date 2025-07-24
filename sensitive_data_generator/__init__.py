04c394fd324cd10a1a61a908741caccd3c3f1a77
Justin21523 <b0979865617@gmail.com>
2025-07-25 00:19:38 +0800

    Export MultiFormatDatasetGenerator in package __init__

--- sensitive_data_generator/__init__.py
+++ sensitive_data_generator/__init__.py
@@ -6,10 +6,11 @@ from .formatters import DataFormatter
 from .advanced_formatters import AdvancedDataFormatter
 from .file_writers import FileWriter
 from .advanced_file_writers import AdvancedFileWriter
+from .dataset_generator import MultiFormatDatasetGenerator
 
 __all__ = [
     "TAIWAN_LOCATIONS","STREET_NAMES","SURNAMES","GIVEN_NAMES",
     "HOSPITALS","MEDICAL_SPECIALTIES",
     "PIIGenerator","DataFormatter","FileWriter",
-    "AdvancedDataFormatter","AdvancedFileWriter"
+    "AdvancedDataFormatter","AdvancedFileWriter", "MultiFormatDatasetGenerator"
 ]
