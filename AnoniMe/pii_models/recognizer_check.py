0c546154986c0f01a4fed5284635b1cea1c28876
Lucas-Chu-0209 <lucasauriant0209@gmail.com>
2025-08-14 20:31:36 +0800

    Delete AnoniMe directory

--- AnoniMe/pii_models/recognizer_check.py
+++
@@ -1,7 +0,0 @@
-# run_recognizers_check.py
-
-from presidio_detector import analyzer
-
-print("=== Recognizers in registry ===")
-for r in analyzer.registry.recognizers:
-    print(f"{r.name}: {r.supported_entities}")
